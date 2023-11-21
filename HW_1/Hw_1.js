239d973688293376af1ce3c9e1ddf8f0ebdfc2bf
РС <KasiaKasilov@gmail.com>
2023-11-21 19:32:04 +0200

    final

---
+++ HW_1/Hw_1.js
@@ -0,0 +1,54 @@
+let hello = 'hello';
+let owu = 'owu';
+let com = 'com';
+let num1 = 1;
+let num10 = 10;
+let num_999 = -999;
+let num123 = 123;
+let numPI = 3.14;
+let numFloat = 2.7;
+let num16 = 16;
+let bool_T = true;
+let bool_F = false;
+
+console.log(hello);
+console.log(owu);
+console.log(num1);
+console.log(com);
+console.log(num10);
+console.log(num_999);
+console.log(num123);
+console.log(numPI);
+console.log(numFloat);
+console.log(num16);
+console.log(bool_T);
+console.log(bool_F);
+console.log('-----------------');
+
+
+let firstName = 'Роман ';
+let middleName = 'Касілов ';
+let lastName = 'Валерійович';
+console.log(middleName + firstName + lastName);
+console.log('-----------------');
+
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a, b, c);
+console.log('-----------------');
+
+let first_name = prompt("Ім'я", []);
+console.log('name ' + first_name);
+let last_name = prompt('По-Батькові', []);
+console.log('last name ' + last_name);
+let age = prompt('Вік', []);
+console.log('age ' + age);
+
+
+
+
+
+
+
